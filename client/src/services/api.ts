eed8a846a3133a89bad3fa025cd834da178a33ce
Prasad <prassadkorgaonkar777@gmail.com>
2026-04-27 01:36:29 +0530

    todo-app flow 2

--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { TodoType } from "../types/todo";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
 });
 
 export const getTodos = async (): Promise<TodoType[]> => {
